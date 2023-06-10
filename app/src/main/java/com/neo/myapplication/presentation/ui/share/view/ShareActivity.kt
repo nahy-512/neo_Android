7fb912c1a620ffeb9c3bce3905be323b0718e1ac
아딘컴\김아린 <jyandysys1@naver.com>
2023-06-11 05:27:05 +0900

    feat: ShareActivity BackButton 구현

--- app/src/main/java/com/neo/myapplication/presentation/ui/share/view/ShareActivity.kt
+++ app/src/main/java/com/neo/myapplication/presentation/ui/share/view/ShareActivity.kt
@@ -1,8 +1,10 @@
 package com.neo.myapplication.presentation.ui.share.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.neo.myapplication.databinding.ActivityShareBinding
+import com.neo.myapplication.presentation.ui.alarm.view.AlarmActivity
 
 class ShareActivity : AppCompatActivity() {
     private lateinit var binding : ActivityShareBinding
@@ -12,7 +14,13 @@ class ShareActivity : AppCompatActivity() {
         binding = ActivityShareBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        onBackButton()
 
     }
 
+    private fun onBackButton() {
+        binding.actShareIvBack.setOnClickListener {
+            startActivity(Intent(this, AlarmActivity::class.java))
+        }
+    }
 }
