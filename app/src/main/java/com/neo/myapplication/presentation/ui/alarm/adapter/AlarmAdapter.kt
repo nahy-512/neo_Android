4416aca3454565fbfc17edc77279f6fbb96f3cc4
아딘컴\김아린 <jyandysys1@naver.com>
2023-06-11 03:08:13 +0900

    feat: 알람 item 클릭시 ShareActivity 이동

--- app/src/main/java/com/neo/myapplication/presentation/ui/alarm/adapter/AlarmAdapter.kt
+++ app/src/main/java/com/neo/myapplication/presentation/ui/alarm/adapter/AlarmAdapter.kt
@@ -1,14 +1,20 @@
 package com.neo.myapplication.presentation.ui.alarm.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.neo.myapplication.databinding.ItemActAlarmRvAlarmsBinding
+import com.neo.myapplication.presentation.ui.share.view.ShareActivity
+import kotlin.reflect.KFunction1
 
 class AlarmAdapter(private val onClick : (Int) -> Unit) : RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder>() {
     inner class AlarmViewHolder(private val binding: ItemActAlarmRvAlarmsBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind() {
-            binding.root.setOnClickListener { onClick.invoke(0) }
+            binding.root.setOnClickListener {
+                binding.root.setOnClickListener { onClick.invoke(0) }
+            }
         }
     }
 
--- app/src/main/java/com/neo/myapplication/presentation/ui/alarm/view/AlarmActivity.kt
+++ app/src/main/java/com/neo/myapplication/presentation/ui/alarm/view/AlarmActivity.kt
@@ -3,9 +3,12 @@ package com.neo.myapplication.presentation.ui.alarm.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.neo.myapplication.databinding.ActivityAlarmBinding
 import com.neo.myapplication.presentation.ui.alarm.adapter.AlarmAdapter
 import com.neo.myapplication.presentation.ui.main.view.MainActivity
+import com.neo.myapplication.presentation.ui.share.view.ShareActivity
 
 class AlarmActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAlarmBinding
@@ -18,6 +21,12 @@ class AlarmActivity : AppCompatActivity() {
         binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val recyclerView: RecyclerView = binding.actAlarmRvContent
+        val itemList: List<String> = getListData()
+
+        recyclerView.adapter = alarmAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         initAlarm()
 
 
@@ -27,10 +36,14 @@ class AlarmActivity : AppCompatActivity() {
         binding.actAlarmRvContent.adapter = alarmAdapter
     }
 
-    private fun onAlarmClicked(positionIdx : Int) {
-        startActivity(Intent(this, MainActivity::class.java))
+    private fun onAlarmClicked(position : Int) {
+        startActivity(Intent(this, ShareActivity::class.java))
     }
 
+
+    private fun getListData() : List<String> {
+        return listOf()
+    }
 
 
 }
